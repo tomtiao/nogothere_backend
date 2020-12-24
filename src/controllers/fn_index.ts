b5fc10f53d915478fe6ce5b967f75c2de78cb21f
tomtiao <tomtiao1234@gmail.com>
2020-12-24 21:20:19 +0800

    map index to '/'

--- src/controllers/fn_index.ts
+++ src/controllers/fn_index.ts
@@ -1,25 +1,16 @@
 import { promises as fs } from 'fs';
 import { ControllerFunc } from '../definition/controller.js';
 import path from 'path';
-import template from '../template.js';
 
 const ROOT = path.resolve(process.argv[2] || '.');
 const FRONTEND_DIR = '/dist/tomtiao.github.io';
-const VIEWS = '/views/';
 
 const fn_index: ControllerFunc = async function (request, response): Promise<void> {
-    const views_path = path.join(ROOT, FRONTEND_DIR, VIEWS);
+    const index_path = path.join(ROOT, FRONTEND_DIR);
 
     try {
-        const base_file = await fs.readFile(path.join(views_path, 'base.html'));
-        let base_html = base_file.toString();
-        
-        const data = {
-            'title': 'foo',
-            'content': `Process ${request.method} ${request.url}.`
-        };
-
-        base_html = template.render(base_html, data);
+        const base_file = await fs.readFile(path.join(index_path, 'index.html'));
+        const base_html = base_file.toString();
 
         response.writeHead(200, { 'Content-Type': 'text/html' });
         response.end(base_html);
